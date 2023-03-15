1b5c5b0585ae8696b0a149bbe24fe958bf910519
Jaime Aza <jaime.aza@decimetrix.com>
2023-03-15 02:19:17 -0500

    Add card clasificador cnn

--- src/components/Card/cardStyle.jsx
+++ src/components/Card/cardStyle.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const CardContainer = styled.section`
+export const CardContainer = styled.article`
   display: flex;
   flex-direction: column;
   justify-content: center;
